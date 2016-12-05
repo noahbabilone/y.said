ae6a62ed0066036f693d50f9309f8c3d64135d65
noahbabilone <noahbabilone@gmail.com>
2016-12-05 11:18:36 +0100

    Updated .gitignore

---
+++ src/YSBundle/Controller/YSController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace YSBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class YSController extends Controller
+{
+     /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('PortfolioBundle:Pages:index.html.twig');
+    }
+    /**
+     * @Route("/contact", name="contactPage")
+     */
+    public function HomeAction()
+    {
+        return $this->render('PortfolioBundle:Pages:index.html.twig');
+    }
+}
